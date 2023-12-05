ef941395ebb15aeb104d7f0444c31714d0df2d1b
Mesay-AK <131782426+Mesay-AK@users.noreply.github.com>
2023-12-06 00:44:33 +0300

    Added solution for Shuffle String

---
+++ Python/Week1/shuffle-string.py
@@ -0,0 +1,7 @@
+class Solution:
+    def restoreString(self, s: str, indices: List[int]) -> str:
+        result=''
+        for i in range(len(s)):
+            
+            result+=(s[indices.index(i)])
+        return result
