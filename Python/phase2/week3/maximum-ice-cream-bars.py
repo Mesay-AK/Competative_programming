a1c27b09dd85017900ddba4f414160c039c5347a
Mesay-AK <131782426+Mesay-AK@users.noreply.github.com>
2024-01-03 17:52:33 +0300

    Added solution for Maximum Ice Cream Bars

--- Python/phase2/week3/maximum-ice-cream-bars.py
+++ Python/phase2/week3/maximum-ice-cream-bars.py
@@ -1,18 +1,24 @@
 class Solution:
     def maxIceCream(self, costs: List[int], coins: int) -> int:
 
-
-        maxim=0
-
-        costs.sort()
-
+        maxim=max(costs)
+        
+        arr=[0]*(maxim+1)
+        total=0
         for i in range(len(costs)):
-            if coins-costs[i]>=0:
-                maxim+=1
-                coins-=costs[i]
-            else:
-                break
-        return maxim
+            arr[costs[i]]+=1
+        
+        
+        for i in range(1,len(arr)):
+            j=0
+            while arr[i]>0 and j<arr[i] and coins-i>=0:
+                coins-=i
+                total+=1
+                j+=1
+
+
+
+        return total
 
 
 
