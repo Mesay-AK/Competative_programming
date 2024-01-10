a25c3a135aa904ac39c279dcc217178bba4aed16
Mesay-AK <131782426+Mesay-AK@users.noreply.github.com>
2024-01-10 21:36:53 +0300

    Add solution for Sort Colors

--- Python/phase2/week4/leetcode/sort-colors.py
+++ Python/phase2/week4/leetcode/sort-colors.py
@@ -1,10 +1,18 @@
 class Solution:
-    def sortColors(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        for j in range(len(nums)):
-            for i in range(len(nums)-1):
-                if nums[i] > nums[i+1]:
-                    nums[i],nums[i+1]=nums[i+1],nums[i]
-        
+    def sortColors(self, arr: List[int]) -> None:
+        n = len(arr)
+        start = 0
+        end = len(arr)-1
+        i = 0
+
+        while i <= end:
+            
+            if arr[i] == 0:
+                arr[i], arr[start] = arr[start], arr[i]
+                start += 1
+            
+            elif arr[i] == 2:
+                arr[i], arr[end] = arr[end], arr[i]
+                end -= 1
+                i -= 1
+            i += 1
