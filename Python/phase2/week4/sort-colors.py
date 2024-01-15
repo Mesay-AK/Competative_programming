3cb7bf0cb7b0b2fbcbcfdc907d84a335aead8351
Mesay-AK <131782426+Mesay-AK@users.noreply.github.com>
2024-01-15 20:06:29 +0300

    Added solution for Sort Colors

---
+++ Python/phase2/week4/sort-colors.py
@@ -0,0 +1,17 @@
+class Solution:
+    def sortColors(self, arr: List[int]) -> None:
+        n = len(arr)
+        start = 0
+        end = len(arr)-1
+        i = 0
+
+        while i <= end:
+            if arr[i] == 0:
+                arr[i], arr[start] = arr[start], arr[i]
+                start += 1
+            
+            elif arr[i] == 2:
+                arr[i], arr[end] = arr[end], arr[i]
+                end -= 1
+                i -= 1
+            i += 1
